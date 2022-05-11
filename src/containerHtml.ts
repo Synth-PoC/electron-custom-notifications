6481e26a5031d6e1e2d17c6a97881969d40661f2
Sai santosh <santosh@usesynth.com>
2022-05-11 16:42:10 +0530

    removed css reset

--- src/containerHtml.ts
+++ src/containerHtml.ts
@@ -96,54 +96,6 @@ const containerHtml =
       }
     </style>
     <style>
-      /* CSS Reset */
-      *,
-      *::before,
-      *::after {
-        box-sizing: border-box;
-      }
-
-      * {
-        margin: 0;
-      }
-
-      html,
-      body {
-        height: 100%;
-      }
-
-      body {
-        line-height: 1.5;
-        -webkit-font-smoothing: antialiased;
-      }
-
-      img,
-      picture,
-      video,
-      canvas,
-      svg {
-        display: block;
-        max-width: 100%;
-      }
-
-      input,
-      button,
-      textarea,
-      select {
-        font: inherit;
-      }
-
-      p,
-      h1,
-      h2,
-      h3,
-      h4,
-      h5,
-      h6 {
-        overflow-wrap: break-word;
-      }
-    </style>
-    <style>
       @charset "UTF-8";
       :root {
         --animate-duration: 1s;
